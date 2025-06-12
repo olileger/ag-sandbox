ee1a091f83dd6ac11103fe4a9217a123a5b2dfea
Olivier <oliv.leger@gmail.com>
2025-06-12 12:08:53 +0200

    Demo MCP Workbench

--- src/TollingMCPWrkbnch.py
+++ src/TollingMCPWrkbnch.py
@@ -7,7 +7,7 @@ async def main():
     from autogen_agentchat.agents import AssistantAgent
     from autogen_agentchat.ui import Console
     from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
-    from autogen_ext.tools.mcp import StreamableHttpServerParams, StreamableHttpMcpToolAdapter, McpWorkbench
+    from autogen_ext.tools.mcp import StreamableHttpServerParams, McpWorkbench
     from datetime import timedelta
     import yaml
 
@@ -21,6 +21,7 @@ async def main():
         terminate_on_close=True,
     )
 
+    # Test d'accès aux outils du MCP
     async with McpWorkbench(server_params=server_params) as wb:
         tools = await wb.list_tools()
         print(yaml.dump(tools, sort_keys=False, default_flow_style=False))
@@ -28,5 +29,22 @@ async def main():
         print(tr.result[0].content)
 
 
+    # Et c'est parti pour le show...
+    llm = AzureOpenAIChatCompletionClient(azure_deployment="aif-gpt-4.1",
+                                          model="gpt-4.1",
+                                          api_version="2024-06-01",
+                                          azure_endpoint=os.getenv("AOAI_ENDPOINT"),
+                                          api_key=os.getenv("AOAI_API_KEY"))
+    
+    async with McpWorkbench(server_params=server_params) as wb:
+        agent = AssistantAgent(name="Cryptographe",
+                               model_client=llm,
+                               workbench=wb,
+                               system_message="""
+                                Tu es un expert en cryptographie.
+                                Tu t'appuis sur tes outils pour répondre aux questions de l'utilisateur.
+                                """)
+
+        await Console(agent.run_stream(task="Génère une phrase de 5 mots puis calcul son hash SHA3-256."))
 
 run(main)
